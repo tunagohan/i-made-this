a87743baea6aeac2d2d76d13b1181381c89334eb
k-waragai <kazuxtuna@gmail.com>
2018-07-10 22:33:17 +0900

    日本語化対応やroutesの設定など初期設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,34 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # トップパス
+  root 'top_pages#index'
+
+  # 見て見て
+  resources :items
+
+  # 全ユーザーの見てみてが見れるタイムライン
+
+  get 'timeline', to: "timelines#index"
+
+  get 'api/v1/timelines', to: "api/timelines#item"
+
+  devise_for :users, controllers: {
+   registrations: 'users/registrations',
+   sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    # ログイン
+    get "login", to: "users/sessions#new"
+    post "login", to: "users/sessions#create"
+    # ログアウト
+    get "logout", to: "users/sessions#destroy"
+    # 登録
+    get "sign_up", to: "users/sessions#new"
+    post "sign_up", to: "users/sessions#create"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # development時にのみ有効のメール認証
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
