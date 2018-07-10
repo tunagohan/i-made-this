ab7c4460f366aefc19b0a0bba55e72ce2e4e6b16
k-waragai <kazuxtuna@gmail.com>
2018-07-11 01:36:22 +0900

    topページにテンプレート仕込み

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,8 @@
 //= link_tree ../images
+//= link_tree ../images/fullsize
+//= link_tree ../images/thumbnails
+
 //= link_directory ../javascripts .js
+//= link_directory ../javascripts/plugins .js
+
 //= link_directory ../stylesheets .css
