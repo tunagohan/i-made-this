de446edcb5ae17a3d1162298655fbec0318d820b
k-waragai <kazuxtuna@gmail.com>
2018-07-10 22:41:14 +0900

    その為root系

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # ログイン時のパス
+  def after_sign_in_path_for(resource)
+    items_path
+  end
+
+  # ログアウト時のパス
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
