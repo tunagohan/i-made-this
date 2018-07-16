fdcfd18b2c7636ee296ef0642fe5c34f5db4a634
k-waragai <kazuxtuna@gmail.com>
2018-07-16 16:03:27 +0900

    設定を諸々変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,25 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: ENV['FOG_PROVIDER'],
-    aws_access_key_id: ENV['FOG_ACCESS_KEY'],
-    aws_secret_access_key: ENV['FOG_SECRET_ACCESS_KEY'],
-    region: ENV['FOG_REGION'],
-    path_style: true
-  }
+  # ENVで扱うものは文字列として扱われるため文字列比較をする必要があった
+  if ENV['FOG_USE'] === 'false'
+    config.storage = :file
+  else
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: ENV['FOG_PROVIDER'],
+      aws_access_key_id: ENV['FOG_ACCESS_KEY'],
+      aws_secret_access_key: ENV['FOG_SECRET_ACCESS_KEY'],
+      region: ENV['FOG_REGION'],
+      path_style: true
+    }
 
-  config.fog_directory = ENV['FOG_S3_BUCKET_NAME']
-  config.asset_host = ENV['FOG_S3_URL']
+    config.fog_directory = ENV['FOG_S3_BUCKET_NAME']
+    config.asset_host = ENV['FOG_S3_URL']
 
-  config.fog_public = false
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
-  config.storage = :fog
+    config.fog_public = false
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+    config.storage = :fog
+  end
 end
+
 # 日本語の文字化けを防ぐ
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
