7ccd846267c49965bc1c18f22d668ba84258b47a
k-waragai <kazuxtuna@gmail.com>
2018-07-22 21:04:45 +0900

    前ディレクトリ削除

--- dev-tool/rspec.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Railsのサーバーを立ち上げます
-# 環境変数に WEB_PORT があることが前提です
-# STOP は Ctrl + c
-
-bundle exec rails db:migrate RAILS_ENV=test
-bundle exec rspec
--- dev-tool/start-rails-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Railsのサーバーを立ち上げます
-# 環境変数に WEB_PORT があることが前提です
-# STOP は Ctrl + c
-
-bundle exec rails s -b 0.0.0.0 -p $WEB_PORT
