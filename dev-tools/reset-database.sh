baa8cc3207ac4767bc621a3fd970657132e687be
k-waragai <kazuxtuna@gmail.com>
2018-07-29 16:32:49 +0900

    test用も追加

--- dev-tools/reset-database.sh
+++ dev-tools/reset-database.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# dev
 bundle exec rake db:drop
 bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rake db:seed
+
+# test
+bundle exec rake db:drop RAILS_ENV=test
+bundle exec rake db:create RAILS_ENV=test
+bundle exec rake db:migrate RAILS_ENV=test
+bundle exec rake db:seed:category RAILS_ENV=test
